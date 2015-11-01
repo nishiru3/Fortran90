a8992cda4aff419e3b9f7bd5274b8bb3f62e6696
nishiru3 <nishiru3>
2015-11-01 15:56:41 +0900

    Add Advection

---
+++ Advection/FirstOrderUpwind.f90
@@ -0,0 +1,12 @@
+PROGRAM Main
+    INTEGER i
+    INTEGER imax
+    INTEGER tmax
+    REAL(8),ALLOCATABLE ::  f(:),fn(:)
+    REAL(8) dx,dt,u
+    
+END PROGRAM Main
+
+SUBROUTINE Input(dx,dt,u)
+    
+END SUBROUTINE Input
